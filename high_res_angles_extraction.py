95d2e52ecea70d8da328dc6e26e624290259cd32
karthikmasi <karthik.ramadass@vanderbilt.edu>
2021-08-05 11:51:56 -0500

    Computes unit normal vectors and started the dot product code block

--- high_res_angles_extraction.py
+++ high_res_angles_extraction.py
@@ -43,6 +43,21 @@ def compute_normal(surf):
     points_np = vtk.util.numpy_support.vtk_to_numpy(points.GetData())
     return normals_np,points_np
 
+def make_vox_normal_vector(normals,points,img):
+    """
+    Converts vtk format coordinates(mm) to voxel space and returns unit normal vectors
+    """
+    normals_surface_np = points + normals
+    normals_vox = trimeshpy.vtk_util.vtk_to_vox(normals_surface_np,img)
+    surface_vox = trimeshpy.vtk_util.vtk_to_vox(points,img)
+    normal_vector_vox = np.zeros(normals_vox.shape)
+    normal_vector_vox = np.zeros(normals_vox.shape)
+    for i in range(len(normal_vector_vox)):
+        normal_vector_vox[i] = \
+          (normals_vox[i] - surface_vox[i])/np.linalg.norm(normals_vox[i]- surface_vox[i])
+    return normal_vector_vox
+
+
 def extract(args):
     """
     Main code block
@@ -50,6 +65,9 @@ def extract(args):
     img, surf = load_img_n_surface(args.img_fn,args.surf_fn)
     tesselated_surf = tesselate_surface(surf,img)
     normals, points = compute_normal(surf)
+    normal_vector = make_vox_normal_vector(normals,points,img)
+    dot_product_angles = compute_dot_product_with_z(normal_vector)
+
 
 def add_to_parser():
     """
