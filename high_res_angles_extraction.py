539a7f48b00c4d5163e2a24f636ebfc68f0a38b5
karthikmasi <karthik.ramadass@vanderbilt.edu>
2021-08-05 10:57:12 -0500

    Parser args initiated

--- high_res_angles_extraction.py
+++ high_res_angles_extraction.py
@@ -6,3 +6,19 @@ import numpy as np
 import nibabel as nib
 import trimeshpy
 import math
+import argparse
+
+def add_to_parser():
+    """
+    Arguments reader
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--img",dest=img_fn, default=None,required=True, \
+                        help="Path of 7T SWI image to register")
+    parser.add_argument("--surface",dest=surf_fn,default=None,required=True,\
+                        help="Path of surface registered to 7T space")
+    return parser
+
+if __name__== '__main__':
+    parser = add_to_parser()
+    OPTIONS = parser.parse_args()
